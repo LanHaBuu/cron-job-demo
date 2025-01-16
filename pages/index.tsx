94feffde817d9616b8a2440fcb643425fd27cfa7
Biu <mac@192.168.1.9.non-exists.ptr.local>
2025-01-17 00:16:52 +0700

    update

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,31 +2,12 @@
 import { useEffect, useState } from "react";
 
 export default function Home() {
-  const [news, setNews] = useState<any>(null)
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch("https://cron-job-demo.vercel.app/api/news");
-        const result = await response.json();
-        setNews(result);
-      } catch (error) {
-        console.error("Error fetching news:", error);
-      }
-    };
-
-    fetchData();
-  },[]);
+ 
 
   return (
     <div>
-      {news?.map((item:any,index:any) => {
-        return (
-          <div key={index}>
-            <p>{item?.id}</p>
-            <p>{item?.name}</p>
-          </div>
-        )
-      })}
+      hello
+    
     </div>
   );
 }
